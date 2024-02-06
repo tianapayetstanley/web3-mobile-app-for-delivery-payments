aa3a8b53f5234a3ee6d680dcc27bdc1d07052634
Abdi Mussa <abdimussa87@gmail.com>
2024-02-06 22:29:25 +0300

    feat: setup hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_TESTNET_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
