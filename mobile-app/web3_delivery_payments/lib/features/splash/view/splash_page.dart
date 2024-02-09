88e90adfe255a109604b177780fffbacab87943f
Abdi Mussa <abdimussa87@gmail.com>
2024-02-09 20:58:01 +0300

    feat: setting up the splash page

--- mobile-app/web3_delivery_payments/lib/features/splash/view/splash_page.dart
+++ mobile-app/web3_delivery_payments/lib/features/splash/view/splash_page.dart
@@ -5,6 +5,54 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF4ade80),
+              Color(0xFF3b82f6),
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Spacer(),
+              Icon(
+                Icons.vpn_lock,
+                size: 100.0,
+                color: Colors.white,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Web3 Based Delivery Payment',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'Prepare to dive into the future',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
