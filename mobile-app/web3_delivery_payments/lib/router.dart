3000378e96687ba41f4f88ca2298772ab3ede782
Abdi Mussa <abdimussa87@gmail.com>
2024-02-09 20:41:03 +0300

    feat: setup routes

---
+++ mobile-app/web3_delivery_payments/lib/router.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:web3_delivery_payments/features/home/view/home_page.dart';
+import 'package:web3_delivery_payments/features/login/view/login_page.dart';
+import 'package:web3_delivery_payments/features/splash/view/splash_page.dart';
+
+class MyRouter {
+  static String splashRouteName = '/splash';
+  static String homeRouteName = '/';
+  static String loginRouteName = '/login';
+
+  static GoRouter router = GoRouter(
+    debugLogDiagnostics: kDebugMode,
+    initialLocation: '/splash',
+    routes: [
+      GoRoute(
+        name: splashRouteName,
+        path: '/splash',
+        pageBuilder: (context, state) {
+          return const NoTransitionPage(child: SplashPage());
+        },
+      ),
+      GoRoute(
+        name: loginRouteName,
+        path: '/login',
+        builder: (context, state) {
+          return const LoginPage();
+        },
+      ),
+      GoRoute(
+        name: homeRouteName,
+        path: '/',
+        pageBuilder: (context, state) => CustomTransitionPage<void>(
+          key: state.pageKey,
+          child: const HomePage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(opacity: animation, child: child),
+        ),
+      ),
+    ],
+    errorBuilder: (context, state) => Scaffold(
+      body: Center(
+        child: Text(
+          state.error == null ? 'Something went wrong' : state.error.toString(),
+        ),
+      ),
+    ),
+    redirect: (context, state) {
+      //Returns null to say you are not redirecting.
+      return null;
+    },
+  );
+}
