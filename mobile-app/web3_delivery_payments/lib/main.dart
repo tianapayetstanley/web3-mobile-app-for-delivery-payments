b9035d350f95d39a72a87fda36cddfd9b72842e1
Abdi Mussa <abdimussa87@gmail.com>
2024-02-10 23:48:24 +0300

    feat: font changed

--- mobile-app/web3_delivery_payments/lib/main.dart
+++ mobile-app/web3_delivery_payments/lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:web3_delivery_payments/common/blocs/bloc_observer.dart';
+import 'package:web3_delivery_payments/common/repositories/smart_contract_repository.dart';
 import 'package:web3_delivery_payments/router.dart';
 
 void main() {
+  EquatableConfig.stringify = kDebugMode;
+  Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
 
@@ -10,10 +19,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Web3 Based Delivery Payment',
-      debugShowCheckedModeBanner: false,
-      routerConfig: MyRouter.router,
+    return ChangeNotifierProvider(
+      create: (context) => SmartContractRepository(),
+      child: MaterialApp.router(
+        title: 'Web3 Based Delivery Payment',
+        debugShowCheckedModeBanner: false,
+        routerConfig: MyRouter.router,
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
+      ),
     );
   }
 }
